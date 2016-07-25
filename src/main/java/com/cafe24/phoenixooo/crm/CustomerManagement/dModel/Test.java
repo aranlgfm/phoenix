dab0990f41615ec56fb2680dc6841ed64373af74
aran <aranlgfm@gmail.com>
2016-07-25 12:24:16 +0900

    [구아란] repository&model 패키지 생성 및 test파일을 임시로 추가해둠

---
+++ src/main/java/com/cafe24/phoenixooo/crm/CustomerManagement/dModel/Test.java
@@ -0,0 +1,5 @@
+package com.cafe24.phoenixooo.crm.CustomerManagement.dModel;
+
+public class Test {
+
+}
---
+++ src/main/java/com/cafe24/phoenixooo/crm/stockManagement/cRepository/Test.java
@@ -0,0 +1,5 @@
+package com.cafe24.phoenixooo.crm.stockManagement.cRepository;
+
+public class Test {
+
+}
