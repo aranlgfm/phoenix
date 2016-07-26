04e83a7223a9f1d45fa5e4b5072a452442dac39d
Shin BogKi <bogki1030@gmail.com>
2016-07-26 09:25:14 +0900

    [신복기]mapper 파일 만들던 도중 db테스트를 위해서 일단 commit. 이후에 다시 진행.

--- src/main/java/com/cafe24/phoenixooo/community/dModel/Article.java
+++ src/main/java/com/cafe24/phoenixooo/community/dModel/Article.java
@@ -1,5 +1,44 @@
 package com.cafe24.phoenixooo.community.dModel;
 
-public class Article {
-
+public class Article 
+{
+	private int articleCode;
+	private String articleTitle;
+	private String articleContent;
+	private String articleDate;
+	private String articleWriter;
+	
+	public int getArticleCode() {
+		return articleCode;
+	}
+	public void setArticleCode(int articleCode) {
+		this.articleCode = articleCode;
+	}
+	public String getArticleTitle() {
+		return articleTitle;
+	}
+	public void setArticleTitle(String articleTitle) {
+		this.articleTitle = articleTitle;
+	}
+	public String getArticleContent() {
+		return articleContent;
+	}
+	public void setArticleContent(String articleContent) {
+		this.articleContent = articleContent;
+	}
+	public String getArticleDate() {
+		return articleDate;
+	}
+	public void setArticleDate(String articleDate) {
+		this.articleDate = articleDate;
+	}
+	public String getArticleWriter() {
+		return articleWriter;
+	}
+	public void setArticleWriter(String articleWriter) {
+		this.articleWriter = articleWriter;
+	}
+	
+	
+	
 }
