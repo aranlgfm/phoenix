5d92283005b2fe15e1a5493ef2bce619450e55d1
Shin BogKi <bogki1030@gmail.com>
2016-08-01 15:12:13 +0900

    [신복기] 미용용품 입고내역 리스트 받아오기 / 입고 등록 화면으로 이동 / 입고 등록 처리.commit을 잊어서 한번에 올림

---
+++ src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Model/GoodsStock.java
@@ -0,0 +1,12 @@
+package com.cafe24.phoenixooo.crm.stockManagement.Model;
+
+public class GoodsStock 
+{
+	private String stockCode;
+	private String paymentCode;
+	private String shopCode;
+	private String employeeCode;
+	private String goodsCode;
+	private String stockDate;
+	private int goodsQuantityNumber;
+}
--- src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Service/GoodsStockService.java
+++ src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Service/GoodsStockService.java
@@ -1,12 +1,11 @@
-package com.cafe24.phoenixooo.crm.stockManagement.Repository;
-
+package com.cafe24.phoenixooo.crm.stockManagement.Service;
 
 import java.util.List;
 
 import com.cafe24.phoenixooo.crm.stockManagement.Model.GoodsStockCommand;
 
-public interface GoodsStockDao {
-	//미용용품 입고 처리
+public interface GoodsStockService {
+	//미용용품 입고 등록
 	int insertGoodsStock(GoodsStockCommand goodsStockCommand);
 	//미용용품 입고 내역 가져오기
 	List<GoodsStockCommand> selectGoodsStockList(GoodsStockCommand goodsStockCommand);
