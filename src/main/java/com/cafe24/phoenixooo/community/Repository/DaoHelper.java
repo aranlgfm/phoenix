fa65a43675d03e33734d406539f00c215f150ff9
aran <aranlgfm@gmail.com>
2016-08-02 17:04:46 +0900

    [구아란] DaoHelper 추가

---
+++ src/main/java/com/cafe24/phoenixooo/community/Repository/DaoHelper.java
@@ -0,0 +1,30 @@
+package com.cafe24.phoenixooo.community.Repository;
+
+public class DaoHelper {
+	protected final String ITEMDESIGNCODE = "CRM_ITEM_DESIGN_";
+	protected final String ITEMCODE = "CRM_ITEM_";
+	protected final String USERCODE = "COM_USER_";
+	protected final String SHOPCODE = "CRM_SHOP_";
+	protected final int SEACHRESULTLIMITNUMBER = 5;
+	
+	public int getSEACHRESULTLIMITNUMBER() {
+		return SEACHRESULTLIMITNUMBER;
+	}
+
+	public String getSHOPCODE() {
+		return SHOPCODE;
+	}
+
+	public String getUSERCODE() {
+		return USERCODE;
+	}
+	
+	public String getITEMCODE() {
+		return ITEMCODE;
+	}
+	
+	public String getITEMDESIGNCODE() {
+		return ITEMDESIGNCODE;
+	}
+	
+}
