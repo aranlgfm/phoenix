ea54ec28ded1cedd43521fc0543a35af1e888442
Shin BogKi <bogki1030@gmail.com>
2016-08-03 14:41:45 +0900

    [신복기] 첫 시작 화면,거래처를 등록하는 화면, 거래처 등록 처리.

--- src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Controller/StockManagementSettingController.java
+++ src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Controller/StockManagementSettingController.java
@@ -1,5 +1,44 @@
 package com.cafe24.phoenixooo.crm.stockManagement.Controller;
 
-public class StockManagementSettingController {
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cafe24.phoenixooo.crm.stockManagement.Model.Account;
+import com.cafe24.phoenixooo.crm.stockManagement.Service.StockManagementSettingService;
+
+
+
+@Controller
+public class StockManagementSettingController {
+	
+	@Autowired
+	StockManagementSettingService stockManagementSettingService;
+	
+	//거래처 내역을 보여주고, 거래처 등록 버튼이 있는 화면으로. 그러니까 첫 화면.
+	@RequestMapping(value = "/phoenix/crm/form/stockManagementSetting", method = RequestMethod.GET)
+	public String moveToStockManagementSetting(Model model) 
+	{ 
+		return "/phoenix/crm/stockManagement/stockManagementSetting";
+	}
+	
+	//거래처를 등록하면 화면으로
+	@RequestMapping(value = "/phoenix/crm/form/insertingAccount", method = RequestMethod.GET)
+	public String moveToInsertingAccount(Account account,Model model) 
+	{ 
+		return "/phoenix/crm/stockManagement/insertingAccount";
+	}
+	
+	//거래처 등록 처리
+	@RequestMapping(value = "/phoenix/crm/process/insertAccount", method = RequestMethod.POST)
+	public String insertAccount(Account account,Model model) 
+	{ 
+		account.setShopCode("CRM_SHOP_1");
+		stockManagementSettingService.insertAccount(account);
+		return "redirect:/phoenix/crm/form/stockManagementSetting";
+	}
 }
---
+++ src/main/java/com/cafe24/phoenixooo/crm/stockManagement/Model/Account.java
@@ -0,0 +1,69 @@
+package com.cafe24.phoenixooo.crm.stockManagement.Model;
+
+public class Account {
+	private String accountCode;
+	private String userCode;
+	private String shopCode;
+	private String accountName;
+	private String chargerName;
+	private String cellPhoneNumber;
+	private String phoneNumber;
+	private String faxNumber;
+	private String chargerMemo;
+	public String getAccountCode() {
+		return accountCode;
+	}
+	public void setAccountCode(String accountCode) {
+		this.accountCode = accountCode;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getShopCode() {
+		return shopCode;
+	}
+	public void setShopCode(String shopCode) {
+		this.shopCode = shopCode;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getChargerName() {
+		return chargerName;
+	}
+	public void setChargerName(String chargerName) {
+		this.chargerName = chargerName;
+	}
+	public String getCellPhoneNumber() {
+		return cellPhoneNumber;
+	}
+	public void setCellPhoneNumber(String cellPhoneNumber) {
+		this.cellPhoneNumber = cellPhoneNumber;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getFaxNumber() {
+		return faxNumber;
+	}
+	public void setFaxNumber(String faxNumber) {
+		this.faxNumber = faxNumber;
+	}
+	public String getChargerMemo() {
+		return chargerMemo;
+	}
+	public void setChargerMemo(String chargerMemo) {
+		this.chargerMemo = chargerMemo;
+	}
+	
+	
+}
